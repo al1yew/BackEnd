f9748d2c45ad082c59accf7275d19d29de878584
al1yew <vasifja@code.edu.az>
2022-05-18 17:12:47 +0400

    Published changes

---
+++ Trial_May_18/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Trial_May_18.Controllers
+{
+    public class HomeController
+    {
+        public string FirstEndPoint() {
+            return "helllooooooo";
+        }
+    }
+}
