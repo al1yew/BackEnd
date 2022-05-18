010fca5b7c1c93aac3f62beec351b8825a597c38
al1yew <vasifja@code.edu.az>
2022-05-19 00:06:54 +0400

    Published changes

--- Trial_May_18/Controllers/HomeController.cs
+++ Trial_May_18/Controllers/HomeController.cs
@@ -1,14 +1,15 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Trial_May_18.Controllers
 {
-    public class HomeController
+    public class HomeController : Controller
     {
-        public string FirstEndPoint() {
-            return "helllooooooo";
+        public IActionResult Index() {
+            return View("Index");
         }
     }
 }
