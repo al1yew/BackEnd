6dfbbc5bb05bb94cf5bb647423df916f849611b0
al1yew <vasifja@code.edu.az>
2022-05-19 22:38:52 +0400

    Published changes

--- Trial_Task_May_19/Controllers/CarController.cs
+++ Trial_Task_May_19/Controllers/CarController.cs
@@ -3,10 +3,31 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Trial_Task_May_19.Models;
 
 namespace Trial_Task_May_19.Controllers
 {
     public class CarController : Controller
     {
+        private readonly List<Car> _cars;
+        //carsin modelini yaratdim
+
+        public IActionResult Index(int? id) {
+            //mene axi id gelecek branda basanda, on prineset mne id i ya suda vstavlu ego, i vernu emu hemin id-de duran elementleri
+            //pcm vopros? ptm shto mojet pridti, a mojet i net
+            //eto tot je id kotoriy v startape
+            return View();
+            //estesna vernet mne view i idu v ego view
+        }
+
+        //sonra  construktoru yigiram
+
+        public CarController()
+        {
+            _cars = new List<Car> { 
+                new Car{ Id=1, Name = "e250", BrandId=}
+            };
+        }
     }
 }
+    
--- Trial_Task_May_19/Models/Car.cs
+++ Trial_Task_May_19/Models/Car.cs
@@ -7,5 +7,12 @@ namespace Trial_Task_May_19.Models
 {
     public class Car
     {
+        //estestvenno u cara est property ved ya xochu otobrajat ego po dot notation
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Year { get; set; }
+        public string EngineType { get; set; }
+        public int BrandId { get; set; }
     }
 }
