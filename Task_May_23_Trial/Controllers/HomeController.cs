cc5be5ca3adea8ec582f8395030b24de945e16da
al1yew <vasifja@code.edu.az>
2022-05-23 11:50:32 +0400

    Add project files.

---
+++ Task_May_23_Trial/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Task_May_23_Trial.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+    }
+}
