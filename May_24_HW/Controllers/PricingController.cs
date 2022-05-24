a46d4dffa3889b22a51e076922cf23ad6f0ccb03
al1yew <vasifja@code.edu.az>
2022-05-24 18:30:13 +0400

    Published changes

--- May_24_HW/Controllers/PricingController.cs
+++ May_24_HW/Controllers/PricingController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using May_24_HW.DAL;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,8 +9,16 @@ namespace May_24_HW.Controllers
 {
     public class PricingController : Controller
     {
+        private readonly AppDbContext _context;
+
+        public PricingController(AppDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
+
             return View();
         }
     }
