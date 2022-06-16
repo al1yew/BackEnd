8bb09b3d8e60a33013277f8cec0021bcbbebd054
al1yew <vasifja@code.edu.az>
2022-06-16 15:42:32 +0400

    Published changes

---
+++ Allup/ViewComponents/HeaderViewComponent.cs
@@ -0,0 +1,26 @@
+﻿using Allup.DAL;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Allup.ViewComponents
+{
+    public class HeaderViewComponent : ViewComponent
+    {
+        private readonly AppDbContext _context;
+
+        public HeaderViewComponent(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IViewComponentResult> InvokeAsync() 
+        {
+            IDictionary<string, string> settings = await _context.Settings.ToDictionaryAsync(x => x.Key, y => y.Value);
+
+            return View(await Task.FromResult(settings));
+        }
+    }
+}
