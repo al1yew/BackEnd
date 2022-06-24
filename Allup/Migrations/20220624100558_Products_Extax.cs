77350b7e803c834204b4c2103ca85cdc47be87c6
al1yew <vasifja@code.edu.az>
2022-06-24 14:48:36 +0400

    Published changes

---
+++ Allup/Migrations/20220624100558_Products_Extax.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Allup.Migrations
+{
+    public partial class Products_Extax : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "ExTax",
+                table: "Products",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "money");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "ExTax",
+                table: "Products",
+                type: "money",
+                nullable: false,
+                oldClrType: typeof(int));
+        }
+    }
+}
---
+++ Allup/Migrations/20220624101018_Products_Extax_again.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Allup.Migrations
+{
+    public partial class Products_Extax_again : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "ExTax",
+                table: "Products",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "ExTax",
+                table: "Products",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldNullable: true);
+        }
+    }
+}
