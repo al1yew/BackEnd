f4549f2ab555f3c463a3a8a4bf76dafda4857a89
al1yew <vasifja@code.edu.az>
2022-07-20 22:56:14 +0400

    finished CRUD for Category

--- FirstApi/Controllers/CategoriesController.cs
+++ FirstApi/Controllers/CategoriesController.cs
@@ -59,9 +59,111 @@ namespace FirstApi.Controllers
             //return Created();
         }
 
-        [HttpPut]
-        public async Task<IActionResult> 
+        [HttpGet]
+        [Route("{id?}")]
+        public async Task<IActionResult> Get(int? id)
+        {
+            //burda uje id lazimdi, cunki bilmeliyik, hansi kategoriyani gormek isteyirik!!!
+            //id nin null olmagini yoxlamirig, cunki null gelse, yuxaridaki Gete gedecek, cunki orda id yoxdu, ozu basha dushur!
 
+            return Ok(await _context.Categories.FirstOrDefaultAsync(c => c.Id == id));
+        }
+
+        [HttpPut]
+        [Route("{id?}")]
+        public async Task<IActionResult> Put(int? id, Category category)
+        {
+            if (id == null) return BadRequest("Id is required");
+
+            if (category.Id != id) return BadRequest("Id is not matching to category's Id!");
+
+            Category dbCategory = await _context.Categories.FirstOrDefaultAsync(c => !c.IsDeleted && c.Id == id);
+
+            if (dbCategory == null) return NotFound("Id is wrong!");
+
+            //useri yoxladig, kechirik dbya
+
+            if (category.IsMain)
+            {
+                if (category.Image == null) return BadRequest("Image is required!");
+            }
+            else
+            {
+                if (category.ParentId == null) return BadRequest("You must choose Main category!");
+
+                if (id == category.ParentId) return BadRequest("Id is the same as id of main category!");
+
+                if (!await _context.Categories.AnyAsync(c => c.Id == category.ParentId && c.Id != id && c.IsMain)) return BadRequest("Main category is incorrect!");
+            }
+
+            dbCategory.Name = category.Name.Trim();
+            dbCategory.ParentId = category.ParentId;
+            dbCategory.Image = category.Image;
+            dbCategory.UpdatedAt = DateTime.UtcNow.AddHours(4);
+            dbCategory.IsMain = category.IsMain;
+
+            await _context.SaveChangesAsync();
+
+            return Content("Updated");
+        }
+
+        [HttpDelete]
+        [Route("{id?}")]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null) return BadRequest("Id is required");
+
+            Category dbCategory = await _context.Categories.Include(c => c.Children).FirstOrDefaultAsync(c => !c.IsDeleted && c.Id == id);
+
+            if (dbCategory == null) return NotFound("Id is wrong!");
+
+            if (dbCategory.IsMain && dbCategory.Children != null)
+            {
+                List<Category> children = await _context.Categories.Where(c => c.ParentId == id).ToListAsync();
+                //ushaglari varsa onlari da silmek lazimdi, bunu daha qisa yazmaq lazimdi amma oxunaqli olsun
+                foreach (var item in children)
+                {
+                    item.IsDeleted = true;
+                    item.DeletedAt = DateTime.UtcNow.AddHours(4);
+                }
+            }
+
+            dbCategory.IsDeleted = true;
+            dbCategory.DeletedAt = DateTime.UtcNow.AddHours(4);
+
+            await _context.SaveChangesAsync();
+
+            return Content("Deleted");
+        }
+
+        [HttpOptions]
+        [Route("{id?}")]
+        public async Task<IActionResult> Restore(int? id)
+        {
+            if (id == null) return BadRequest("Id is required");
+
+            Category dbCategory = await _context.Categories.Include(c => c.Children).FirstOrDefaultAsync(c => c.Id == id);
+
+            if (dbCategory == null) return NotFound("Id is wrong!");
+
+            if (dbCategory.IsMain && dbCategory.Children != null)
+            {
+                List<Category> children = await _context.Categories.Where(c => c.ParentId == id).ToListAsync();
+                //ushaglari varsa onlari da silmek lazimdi, bunu daha qisa yazmaq lazimdi amma oxunaqli olsun
+                foreach (var item in children)
+                {
+                    item.IsDeleted = false;
+                    item.DeletedAt = null;
+                }
+            }
+
+            dbCategory.IsDeleted = false;
+            dbCategory.DeletedAt = null;
+
+            await _context.SaveChangesAsync();
+
+            return Content("Restored");
+        }
 
     }
 }
